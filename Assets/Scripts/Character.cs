487b949c7c635bfbde401a1ff8c1fbd8c867d1e6
SuperBuli <65170655+SuperBuli@users.noreply.github.com>
2020-06-09 22:37:06 +0200

    push test
    push test

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int health;
+    public int armor;
+    public int attack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (FirstPlayTheGame())
+        {
+            GeneratePlayerPrefs();
+            Debug.Log("Error");
+        }
+
+        health = PlayerPrefs.GetInt(Constants.Key_Of_Health);
+        armor = PlayerPrefs.GetInt(Constants.Key_Of_Armor);
+        attack = PlayerPrefs.GetInt(Constants.Key_Of_Attack);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void save()
+    {
+        PlayerPrefs.SetInt(Constants.Key_Of_Health, health);
+        PlayerPrefs.SetInt(Constants.Key_Of_Armor, armor);
+        PlayerPrefs.SetInt(Constants.Key_Of_Attack, attack);
+
+        PlayerPrefs.Save();
+    }
+
+    private void GeneratePlayerPrefs()
+    {
+        PlayerPrefs.SetInt(Constants.Key_Of_Health, Constants.InitalHealth);
+        PlayerPrefs.SetInt(Constants.Key_Of_Armor, Constants.InitalArmor);
+        PlayerPrefs.SetInt(Constants.Key_Of_Attack, Constants.InitalAttack);
+
+        PlayerPrefs.Save();
+    }
+
+    private bool FirstPlayTheGame()
+    {
+        return !PlayerPrefs.HasKey(Constants.Key_Of_Health);
+    }
+}
---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿
+public static class Constants
+{
+    public const string Key_Of_Health = "health";
+    public const string Key_Of_Armor = "Armor";
+    public const string Key_Of_Attack = "Attack";
+
+    public const int InitalHealth = 100;
+    public const int InitalArmor = 1;
+    public const int InitalAttack = 5;
+}
