000ed7cbcd3d0ec3a77e23e0a322ca2388a2aea6
SuperBuli <65170655+SuperBuli@users.noreply.github.com>
2020-06-10 14:37:09 +0200

    push test
    push test

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Test : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Dictionary<int, GameObject> _draggingIcons = new Dictionary<int, GameObject>();
+    private Dictionary<int, RectTransform> _draggingPlanes = new Dictionary<int, RectTransform>();
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Canvas _canvas = Helper.FindInParents<Canvas>(gameObject);
+
+        _draggingIcons[eventData.pointerId] = new GameObject("draggingIcons");
+        _draggingIcons[eventData.pointerId].transform.SetParent(_canvas.transform, false);
+        _draggingIcons[eventData.pointerId].transform.SetAsLastSibling();
+
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
