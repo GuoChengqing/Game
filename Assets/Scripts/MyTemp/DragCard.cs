0433eebf6c30b785d30f4a1c3d84d947e36963fc
SuperBuli <65170655+SuperBuli@users.noreply.github.com>
2020-06-23 22:09:14 +0200

    1
    1

--- Assets/Scripts/MyTemp/DragCard.cs
+++ Assets/Scripts/MyTemp/DragCard.cs
@@ -4,32 +4,50 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
-public class DragCard : MonoBehaviour, IDragHandler, IBeginDragHandler
+public class DragCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IDropHandler
 {
+    public Vector2 range = new Vector2(5f, 3f);
     private RectTransform rectTransform;
-    private Vector2 originAnchoredPosition;
+    private Vector2 mRot = Vector2.zero;
+    private Quaternion startQuaternion;
+    private Vector2 startPosition;
+    private Quaternion beforeDragQuaternion;
+    private Vector2 beforeDragPosition;
 
-    public void OnBeginDrag(PointerEventData eventData)
+    void Start()
     {
-        originAnchoredPosition = rectTransform.anchoredPosition;
-
-        throw new System.NotImplementedException();
+        rectTransform = GetComponent<RectTransform>();
+        startQuaternion = rectTransform.localRotation;
+        startPosition = rectTransform.anchoredPosition;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         GetComponent<RectTransform>().anchoredPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        Vector3 pos = Input.mousePosition;
+
+        float halfWidth = Screen.width * 0.5f;
+        float halfHeight = Screen.height * 0.5f;
+        float x = Mathf.Clamp((pos.x - halfWidth) / halfWidth, -1f, 1f);
+        float y = Mathf.Clamp((pos.y - halfHeight) / halfHeight, -1f, 1f);
+        mRot = Vector2.Lerp(mRot, new Vector2(x, y), Time.deltaTime * 5f);
+
+        rectTransform.localRotation = startQuaternion * Quaternion.Euler(-mRot.y * range.y, mRot.x * range.x, 0f);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void OnDrop(PointerEventData eventData)
     {
-        rectTransform = GetComponent<RectTransform>();
+        if (Input.mousePosition.y > Screen.height * 0.3)
+            Debug.Log("nice");
+
+        rectTransform.localRotation = beforeDragQuaternion;
+        rectTransform.anchoredPosition = beforeDragPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        beforeDragQuaternion = rectTransform.localRotation;
+        beforeDragPosition = rectTransform.anchoredPosition;
     }
 }
--- Assets/Scripts/MyTemp/MyTemp.cs
+++ Assets/Scripts/MyTemp/MyTemp.cs
@@ -1,33 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
 
 public class MyTemp : MonoBehaviour
 {
-    public Button button;
+    public GameObject cardPrefab;
+    public GameObject canvas;
+    public int currentCardNumber = 0;
+    public int maxRotateAngle = 60;
+    public float cardWidth;
+    public float cardContainer = 0.2f;
+
+    private List<GameObject> cardList = new List<GameObject>(10);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cardWidth = cardPrefab.GetComponent<RectTransform>().sizeDelta.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && currentCardNumber < 10)
         {
-            button.gameObject.SetActive(true);
-            Debug.Log("OK");
+            currentCardNumber++;
+
+            GameObject card = Instantiate(cardPrefab, new Vector3(0, 50, 0), Quaternion.identity);
+            card.transform.SetParent(canvas.transform, false);
+            cardList.Add(card);
+
+            UpdataCardList();
         }
-        else
+    }
+
+    private void UpdataCardList()
+    {
+        int rotetaStep = 120 / cardList.Count;
+        float xDirectionStep = 0.4f * cardPrefab.GetComponent<RectTransform>().sizeDelta.x;
+        float startX = (Screen.width - xDirectionStep * cardList.Count) / 2;
+        float currentX = startX;
+        float currentRotate = (cardList.Count / 2) * 12;
+
+        for (int i = 0; i < cardList.Count; i++)
         {
-            button.gameObject.SetActive(false);
+            cardList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(currentX, 50);
+            cardList[i].GetComponent<RectTransform>().eulerAngles = new Vector3(0, 0, currentRotate);
+            currentX += xDirectionStep;
+            currentRotate -= 12;
         }
-
-
     }
 }
